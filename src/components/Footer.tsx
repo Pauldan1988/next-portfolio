8eed39c3d063fb5ff99ac6f08ea3ab9e0fb01f12
Paul Dangelo <pdangelo1988@gmail.com>
2023-09-15 16:51:33 -0400

    Footer almost done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,43 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const Footer = function () {
+
+export default function Footer() {
     return (
-        <footer className="footer">
-        <p className="footer__text">
-          Made with{' '}
-          <svg
-            datasanity-icon="heart-filled"
-            width="1em"
-            height="1em"
-            viewBox="0 0 25 25"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M17 16C15.8 17.3235 12.5 20.5 12.5 20.5C12.5 20.5 9.2 17.3235 8 16C5.2 12.9118 4.5 11.7059 4.5 9.5C4.5 7.29412 6.1 5.5 8.5 5.5C10.5 5.5 11.7 6.82353 12.5 8.14706C13.3 6.82353 14.5 5.5 16.5 5.5C18.9 5.5 20.5 7.29412 20.5 9.5C20.5 11.7059 19.8 12.9118 17 16Z"
-              fill="currentColor"
-              stroke="currentColor"
-              strokeWidth="1.2"
-            ></path>
-          </svg>{' '}
-          at Sanity
-        </p>
-      </footer>
+        <footer className="flex flex-row gap-2 align-right">
+            <div>
+                <a className="flex flex-row">
+                    <Link href="https://www.github.com/Pauldan1988">
+                        <Image
+                            src="/github.png"
+                            alt="Github Logo"
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                </a>
+                <a className="flex flex-row">
+                    <Link href="https://www.linkedin.com/in/paul-dan-9a0b6b1b9/">
+                        <Image
+                            src="/linkedin.png"
+                            alt="LinkedIn Logo"
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                </a>
+                <a className="flex flex-row">
+                    <Link href="https://www.gmail.com/pdangelo1988@gmail.com">
+                        <Image
+                            src="/gmail.png"
+                            alt="Gmail Logo"
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                </a>
+            </div>
+        </footer>
     )
 }
-
-export default Footer
